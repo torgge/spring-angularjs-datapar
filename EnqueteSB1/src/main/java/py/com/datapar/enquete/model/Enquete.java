028e61e73460e3ccdfcd86e4db00c26903be84a0
lyndontavares <integraldominio@gmail.com>
2015-10-11 12:06:52 -0300

    new

--- EnqueteSB1/src/main/java/py/com/datapar/enquete/model/Enquete.java
+++ EnqueteSB1/src/main/java/py/com/datapar/enquete/model/Enquete.java
@@ -9,6 +9,8 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -53,6 +55,8 @@ public class Enquete {
 	
 	private TipoEnqueteStatus status;
 	
+	@ManyToOne
+	@JoinColumn(name="autor_id")
 	private Participante autor;
 
 	//@JsonIgnore

ae02b2265d5e5afc06475628fcd32aac13b29eaf
lyndontavares <integraldominio@gmail.com>
2015-10-13 13:50:57 -0300

    new

---
+++ Demo01/src/test/java/com/example/Demo01ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Demo01Application.class)
+public class Demo01ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

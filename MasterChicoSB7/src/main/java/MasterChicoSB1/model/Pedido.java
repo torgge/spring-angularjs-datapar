c22b26efcf5779de4ac09e9303d8cf491384381a
lyndontavares <integraldominio@gmail.com>
2015-10-18 10:15:57 -0200

    material design

---
+++ MasterChicoSB7/src/main/java/MasterChicoSB1/model/Pedido.java
@@ -0,0 +1,95 @@
+package MasterChicoSB1.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+/**
+ * Fornecedor
+ * 
+ * @author  Lyndodn Tavares
+ * @version 1.0
+ * 
+ */
+@Entity
+@Table(name="tab_pedido")
+public class Pedido {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	private Date data;
+
+	@ManyToOne
+	@JoinColumn(name="fornecedor_id")
+	private Fornecedor fornecedor;
+	private String situacao;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Fornecedor getFornecedor() {
+		return fornecedor;
+	}
+
+	public void setFornecedor(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+
+	public String getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(String situacao) {
+		this.situacao = situacao;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Pedido [id=" + id + ", data=" + data + ", fornecedor=" + fornecedor + ", situacao=" + situacao
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+	
+}

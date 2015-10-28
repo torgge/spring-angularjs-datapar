5815e3b42f930613381164c4be3a4453de2191b7
lyndontavares <integraldominio@gmail.com>
2015-10-28 17:15:29 -0200

    news

--- app-MasterChico/MasterChicoSB6/src/main/resources/static/app/grafico/grafico-controller.js
+++ app-MasterChico/MasterChicoSB6/src/main/resources/static/app/grafico/grafico-controller.js
@@ -2,6 +2,18 @@
 
 angular.module('app').controller('GraficoController',['$http','$scope','$timeout',function($http,$scope,$timeout){
 	
+
+	//grafico de linhas 
+	$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+	$scope.series = ['Series A', 'Series B'];
+	$scope.data = [
+	    [65, 59, 80, 81, 56, 55, 40],
+	    [28, 48, 40, 19, 86, 27, 90]
+	];
+	$scope.onClick = function (points, evt) {
+	    console.log(points, evt);
+	};
+	
 }]);
 
 

086682056444fe1900253a6a3ee9902bed37ff25
lyndontavares <integraldominio@gmail.com>
2015-10-31 17:55:24 -0200

    news

--- app-MasterChico/MasterChicoSB6b/src/main/resources/static/app/tempo/tempo-controller.js
+++ app-MasterChico/MasterChicoSB6b/src/main/resources/static/app/tempo/tempo-controller.js
@@ -3,6 +3,9 @@ angular.module('app').controller('TempoController',
 
 	var urlBase = 'http://localhost:5000';
 
+	$tempo={temperatura:undefined,icon:'/image/10d.png'};
+
+  /* Via Java
 	$scope.tempo = $http.get( urlBase+'/api/tempo/foz')
 	.success( function(data){
 
@@ -11,5 +14,19 @@ angular.module('app').controller('TempoController',
 		console.log(data);
 
 	});
+  */
+
+	//direto via ajax
+	$scope.tempo = $http.get( 'http://api.openweathermap.org/data/2.5/weather?q=Foz do Iguacu&mode=json&units=metric&appid=bd82977b86bf27fb59a04b61b657fb6f')
+	.success( function(data){
+
+		$scope.tempo.temperatura = data.main.temp;
+		$scope.tempo.icon = 'http://openweathermap.org/img/w/'+data.weather[0].icon+'.png';
+
+		console.log(data);
+		console.log($scope.tempo);
+
+	});
+
 
 }]);
